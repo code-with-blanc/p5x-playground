99ed7ef49f36ca8f8d9c7125eb87652e3cb2c8f5
pedroblancdti <pedro.blanc@dtidigital.com.br>
2020-07-07 10:12:25 -0300

    bundler: it works!

--- src/services/project/types/project.ts
+++ src/services/project/types/project.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-empty-function */
 export default class Project {
   constructor(
     public id: number,
     public name: string,
   ) {}
+
+  public fileIds: number[] = [];
 }
