f73419ba446995cae76076cb02e537ada7df272a
pedroblancdti <pedro.blanc@dtidigital.com.br>
2020-07-07 21:29:44 -0300

    Print SHA on app startup

---
+++ src/commit_number.js
@@ -0,0 +1,2 @@
+// Should be replaced by the commit number in CI
+export default '00000000000000_DEV';
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import commitNumber from './commit_number';
+
+// eslint-disable-next-line no-console
+console.log(`Playground version ${(commitNumber || '').slice(-8)}`);
+
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
