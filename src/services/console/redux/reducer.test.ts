333e702fee302bbb192b0ccf0c1db92edd1623d2
pedroblancdti <pedro.blanc@dtidigital.com.br>
2020-07-16 09:50:48 -0300

    tests

--- src/services/console/redux/reducer.test.ts
+++ src/services/console/redux/reducer.test.ts
@@ -4,11 +4,13 @@ import deepFreeze from 'deep-freeze';
 import reducer from './reducer';
 import * as actions from './actions';
 import { ConsoleState } from './consoleState';
+import { Message } from '../types';
 
 describe('Console reducer', () => {
   it('message type: log', () => {
     const initialState = deepFreeze<ConsoleState>({
       messages: [],
+      nextId: 1,
     }) as ConsoleState;
     
     const result = reducer(
@@ -17,11 +19,23 @@ describe('Console reducer', () => {
     );
 
     expect(result.messages[0].method).toEqual('log');
-    expect(result.messages[0].data).toEqual('my message');
+    expect(result.messages[0].data).toEqual(['my message']);
   });
 
   it('clears console', () => {
-    const testIsWritten = false;
-    expect(testIsWritten).toEqual(true);
+    const initialState = deepFreeze<ConsoleState>({
+      messages: [getInfoMsg('Message 1', 1)],
+      nextId: 2,
+    }) as ConsoleState;
+
+    const result = reducer(initialState, actions.clear());
+
+    expect(result.messages.length).toEqual(0);
   });
 });
+
+const getInfoMsg = (text : string, id: number) => ({
+  id: `${id}`,
+  method: 'info',
+  data: [text],
+} as Message);
